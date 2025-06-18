66c6c6b1f9ae00cb65b9cdf1849ddb263060c419
B00028\B-00028 <erniepeng@gateweb.com.tw>
2025-06-18 23:17:50 +0800

    feat: 新建開發環境配置

---
+++ src/main/java/org/example/mynameless/MyNamelessApplication.java
@@ -0,0 +1,13 @@
+package org.example.mynameless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNamelessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNamelessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mynameless/MyNamelessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mynameless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNamelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
